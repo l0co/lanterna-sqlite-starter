b59e94fbde93fa7b712f66b6bf4ac047f0e6c344
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2020-03-27 07:46:47 +0100

    initial commit

---
+++ src/main/kotlin/com/lifeinide/lanterna/App.kt
@@ -0,0 +1,17 @@
+package com.lifeinide.lanterna
+
+/**
+ * @author Lukasz Frankowski
+ */
+class App {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello world") // TODOLF remove
+        }
+
+    }
+
+}
